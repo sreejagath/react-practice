5cb5821e65d69573510f30834a92b1c2a40bfaac
Jagal Sree <jagalsree007@gmail.com>
2021-05-17 06:52:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import './App.css'
+import Header from './components/header';
+
 function App() {
+  const data='Jagal Ser'
   return (
+    <div className ='hello'>
+    
     <h1>Hello,World!</h1>
+    <Header data={data}/>
+    <p>{data}</p>
+    </div>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+
+class Header extends Component{
+render(){
+
+
+    return(<h1>{this.props.data}</h1>)
+}
+}
+export default Header 
